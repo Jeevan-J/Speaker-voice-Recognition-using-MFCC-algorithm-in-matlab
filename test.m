3c76fbcf09d045b807096cbc3b37ad115a18bf5f
Jeevan Jamakayala <jeevan.jamakayala@gmail.com>
2018-05-26 10:39:58 +0530

    Update test.m
    Now it shows the speaker name on LCD display connected to Arduino board which in turn connected to MatLab. it communicates with MatLab using serial communication. For more info on connecting arduino to matlab and interfacing LCD display look into this article, https://in.mathworks.com/help/supportpkg/arduinoio/ug/add-lcd-library.html

--- test.m
+++ test.m
@@ -1,29 +1,29 @@
 function [] = test(name) 
-% defining a function with name 'test' and input parameter 'name'. name refers to path of location where audio sample is going to store.
-% Suppose Speaker with unknown ID is speaking and I want to know who it is. I execute the fundtion by following command.
-% test('Test/unknown1.wav')
-% Above command saves the audio sample with name 'unknown1.wav' in Test folder
-display('Start Speaking'); % displays the string Start Speaking in command window
-sig = audiorecorder(44100,16,1); % Creates an audio object with 44100 sampling rate, 16-bits and 1-audio channel.
-recordblocking(sig,3); % records audio for 3 secs
-display('Stop Speaking'); % displays the string Stop Speaking in command window
-name1 = getaudiodata(sig); % getting data from audio object as a vector
-audiowrite(name,name1,44100); % saving the audio sample in test folder
+    % defining a function with name 'test' and input parameter 'name'. name refers to path of location where audio sample is going to store.
+    % Suppose Speaker with unknown ID is speaking and I want to know who it is. I execute the fundtion by following command.
+    % test('Test/unknown1.wav')
+    % Above command saves the audio sample with name 'unknown1.wav' in Test folder
+    display('Start Speaking'); % displays the string Start Speaking in command window
+    sig = audiorecorder(44100,16,1); % Creates an audio object with 44100 sampling rate, 16-bits and 1-audio channel.
+    recordblocking(sig,3); % records audio for 3 secs
+    display('Stop Speaking'); % displays the string Stop Speaking in command window
+    name1 = getaudiodata(sig); % getting data from audio object as a vector
+    audiowrite(name,name1,44100); % saving the audio sample in test folder
 
-% refer to mfcc.m file and understand the terms below and assign values as per your requirement 
-Tw=25;
-Ts=10;
-alpha=0.97;
-R = [300 3700];
-M = 20;
-C = 13;
-L = 22;
-hamming = @(N)(0.54-0.46*cos(2*pi*[0:N-1].'/(N-1)));
-dis=zeros(1,9);
-[ tMFCCs, ~, ~ ] = mfcc( name1, 44100, Tw, Ts, alpha, hamming, R, M, C, L );
+    % refer to mfcc.m file and understand the terms below and assign values as per your requirement 
+    Tw=25;
+    Ts=10;
+    alpha=0.97;
+    R = [300 3700];
+    M = 20;
+    C = 13;
+    L = 22;
+    hamming = @(N)(0.54-0.46*cos(2*pi*[0:N-1].'/(N-1)));
+    dis=zeros(1,9);
+    [ tMFCCs, ~, ~ ] = mfcc( name1, 44100, Tw, Ts, alpha, hamming, R, M, C, L );
 
-% After finding MFCC of test sample, we will find MFCC of each speaker model in Train folder and 
-% also Euclidian distance between the speaker model and test sample
+    % After finding MFCC of test sample, we will find MFCC of each speaker model in Train folder and 
+    % also Euclidian distance between the speaker model and test sample
     [speaker1,Fs]=audioread('train/raj.wav');                % Get the data of speaker model to a variable
     [MFCCs1,~,~] = mfcc(speaker1, Fs, Tw, Ts, alpha, hamming, R, M, C, L );% Calculate MFCC of speaker model 
     dis(1) = dtw(tMFCCs,MFCCs1);                                           % find the euclidian distance using "dtw" function and store it in a array
@@ -61,9 +61,16 @@ dis=zeros(1,9);
     [MFCCs9,~,~] = mfcc(speaker9, Fs, Tw, Ts, alpha, hamming, R, M, C, L );
     dis(9) = dtw(tMFCCs,MFCCs9);
     display(dis(9));
-dis1=sort(dis); % since we have stored all euclidian distances in an array 'dis', we will sort the array in ascending order so that our first element value is least euclidian distance.
-spe={'raj','chaitanya','deepak','gella','challa sir','akhil','surya','manoj','sir'}; % we write the speaker ID's in order that we stored MFCC in an array
-for i=1:1:9
-    if(dis1(1)==dis(i)),display(spe(i)); % Now that we know minimum euclidian distance, we can find the Speaker ID with closest matching to the test sample.
+    dis1=sort(dis); % since we have stored all euclidian distances in an array 'dis', we will sort the array in ascending order so that our first element value is least euclidian distance.
+    spe={'raj','chaitanya','deepak','gella','challa sir','akhil','surya','manoj','sir'}; % we write the speaker ID's in order that we stored MFCC in an array
+    for i=1:1:9
+        if(dis1(1)==dis(i))
+            display(spe(i)); % Now that we know minimum euclidian distance, we can find the Speaker ID with closest matching to the test sample.
+            a = arduino('com5','uno','libraries','ExampleLCD/LCDAddon','ForceBuild',true); %connect to arduino board using serial communication
+            lcd = addon(a,'ExampleLCD/LCDAddon',{'D7','D6','D5','D4','D3','D2'}); % See this link for more info: https://in.mathworks.com/help/supportpkg/arduinoio/ug/add-lcd-library.html
+            initializeLCD(lcd);
+            printLCD(lcd,string(spe(i)));
+            clearLCD(lcd);
+        end
     end
 end
